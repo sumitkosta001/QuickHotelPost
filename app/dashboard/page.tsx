b9d38aa5339e0d2dc28c46b84e56e13892bd1688
rutuparna <rutuparnapradhan38@gmail.com>
2025-03-27 20:13:52 +0530

    added all template interface

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,14 +1,19 @@
-import React from 'react'
+"use client"
+import React, { useState } from 'react'
 import SearchSection from './_components/SearchSection'
+import TemplateListSection from './_components/TemplateListSection'
 
 function Dashboard() {
+  const [userSearchInput,setUserSearchInput]=useState<string>()
   return (
     <div>
-      {/*Search Section*/}
-       <SearchSection/>
-      
+      {/* Search Section*/ }
+       <SearchSection onSearchInput={(value:string)=>setUserSearchInput} />
 
-      {/*Template List Section*/}
+      {/* Template List Section*/ }
+      <TemplateListSection userSearchInput={userSearchInput}/>
+
+
     </div>
   )
 }
